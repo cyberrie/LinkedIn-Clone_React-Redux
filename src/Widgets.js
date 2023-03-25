3d21edd338db0425d617b9daf549428d605e3538
helena <helena.gilja@gmail.com>
2023-03-25 20:03:43 +0000

    widget width updated

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,38 +1,37 @@
-import React from 'react'
-import './Widgets.css'
-import { Info } from '@mui/icons-material'
-import { FiberManualRecord } from '@mui/icons-material'
+import React from "react";
+import "./Widgets.css";
+import { Info } from "@mui/icons-material";
+import { FiberManualRecord } from "@mui/icons-material";
 
 function Widgets() {
-
-  const newsArticle = (heading, subtitle ) => {
-
+  const newsArticle = (heading, subtitle) => {
     return (
-
-      <div className='widgets_article'>
-      <div className='widgets_articleLeft'>
-    <FiberManualRecord />
+      <div className="widgets_article">
+        <div className="widgets_articleLeft">
+          <FiberManualRecord />
+        </div>
+        <div className="widgets_articleRight">
+          <h4>{heading}</h4>
+          <p>{subtitle}</p>
+        </div>
       </div>
-      <div className='widgets_articleRight'>
-        <h4>{heading}</h4>
-        <p>{subtitle}</p>
-      </div>
-    </div>
-      )
-  }
+    );
+  };
   return (
-    <div className='widgets'>
-      <div className='widgets_header'>
+    <div className="widgets">
+      <div className="widgets_header">
         <h2>LinkedIn News</h2>
-        <Info/>
+        <Info />
       </div>
-      {newsArticle('Elon Musk sold Twitter', 'Top News - 28 readers ')}
-      {newsArticle('UK Parliament bans TikTok', '1 day ago - 10,786 readers ')}
-      {newsArticle('ChatGPT replaced all Software Devs', 'now - 1 mil. readers ')}
-      {newsArticle('Redux is so cool!', '1 day ago - 1 reader ')}
+      {newsArticle("Elon Musk sold Twitter", "Top News - 28 readers ")}
+      {newsArticle("UK Parliament bans TikTok", "1 day ago - 10,786 readers ")}
+      {newsArticle(
+        "Crafting the perfect resignation letter",
+        "now - 1 mil. readers "
+      )}
+      {newsArticle("Redux is so cool!", "1 day ago - 1 reader ")}
     </div>
-
-  )
+  );
 }
 
-export default Widgets
+export default Widgets;
